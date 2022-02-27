9a336ab66035cbac8feb39799ccb1b0edcba1346
tekashiZiinMaKer <98004421+tekashiZiinMaKer@users.noreply.github.com>
2022-02-27 15:23:42 -0300

    Update test.rb

--- test.rb
+++ test.rb
@@ -38,9 +38,8 @@ while true do
     puts '
     [*] --- https://github.com/tekashiZiinMaKer --- [*]
     '.colorize(:color => :light_blue) 
-puts'=======================' 
-puts'
-[ 1 ] Consulta CEP
+puts'======================='.colorize(:color => :light_blue) 
+puts'[ 1 ] Consulta CEP
 [ 2 ] Consulta DDD
 [ 3 ] Consulta IP
 [ 4 ] Consulta CNPJ'.colorize(:color => :green)
